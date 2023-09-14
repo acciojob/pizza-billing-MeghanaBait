95d2a51d689767003b2d28b1ea99420a38302398
MeghanaBait <meghanawork777@gmail.com>
2023-09-14 12:33:29 +0530

    changes in getBill method

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -55,6 +55,6 @@ public class Pizza {
 
     public String getBill(){
         // your code goes here
-        return this.bill += "Total Price: "+this.price+"\n";
+        return this.bill + "Total Price: "+this.price+"\n";
     }
 }
